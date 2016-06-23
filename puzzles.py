0770603df333501ccd7f3f12b628174c025f2c5f
Juan M Penaranda <jmpo1618@gmail.com>
2016-06-22 22:33:21 -0400

    Fixed simple Codeforces problem.

--- puzzles.py
+++ puzzles.py
@@ -1,8 +1,8 @@
 nums = [int(i) for i in input().split()]
 pieces = [int(i) for i in input().split()]
 pieces.sort()
-mini = 10001
-for i in range(nums[1] - nums[0]):
+mini = pieces[nums[0] - 1] - pieces[0]
+for i in range(nums[1] - nums[0] + 1):
     dif = pieces[i + nums[0] - 1] - pieces[i]
     if dif < mini:
         mini = dif
